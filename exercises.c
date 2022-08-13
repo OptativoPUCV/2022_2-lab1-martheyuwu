418a998d1be7c6fb4d332892a1c0d7040acfbcc7
Replit user <>
2022-08-13 00:41:54 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -83,7 +83,7 @@ typedef struct {
 
 Vector * crearVector(int n) {
   Vector *v= NULL;
-  v = (Vector*)calloc(n,sizeof(Vector));
+  *v = (Vector*)calloc(n,sizeof(Vector));
   return v;
 }
 
@@ -113,6 +113,7 @@ Función que suma los vectores `a` y `b` y
 actualiza el vector `c` con el resultado de la suma.
 */
 void sumaV(Vector * a, Vector * b, Vector * c) {
+  
 }
 
 /*
