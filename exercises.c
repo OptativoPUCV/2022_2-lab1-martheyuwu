08a287104f180ce8f95e2f343312cfc2bbff2c80
Replit user <>
2022-08-13 00:51:02 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -92,9 +92,8 @@ Ejercicio 5a.
 Programe la función void asignarValor(Vector * v, int i, int valor), 
 la cual asigna el valor a la posición i del vector v.
 */
-//void asignarValor(Vector * v, int i, int valor) {
- // *v[i].datos=valor;
-//}
+void asignarValor(Vector * v, int i, int valor) {
+}
 
 /*
 Ejercicio 6.
